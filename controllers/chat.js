3b3f859d4c4576e0f1d87b92ca7f3cdb31edc36c
talented_vicky <victorotubure7@gmail.com>
2023-08-13 13:42:31 +0100

    fixed chat bugs

--- controllers/chat.js
+++ controllers/chat.js
@@ -10,19 +10,36 @@ const noData = (data, errMsg) => {
     }
 }
 
+// ahmed => 
+// 64d767eec1b864b8d6032672
+// ahmedlarry01@gmail.com
+
+// Ajibola => 
+// 64d74acca903ec8a107a9cb5
+// harjeeborlar@gmail.com
+
 exports.sendChat = async (req, res, next) => {
     const { receiverId } = req.params
     const { name, email, message } = req.body
     
     try {
         const receiver = await User.findById(receiverId)
+        // const receiver = await User.findById("64d767eec1b864b8d6032672")
         noData(receiver, "Recipient Not Found")
 
+        const sender = await User.findById(req.userId)
+        // const sender = await User.findById("64d74acca903ec8a107a9cb5")
+        noData(sender, "Oops, No Message for this user")
+
         const chat = new Chat({ name, email, message, senderId: req.userId, receiverId })
+        // const chat = new Chat({ name, email, message, senderId: "64d74acca903ec8a107a9cb5", receiverId })
         await chat.save()
 
         receiver.messages.push(chat)
+        sender.messages.push(chat)
+
         await receiver.save()
+        await sender.save()
 
         res.status(200).json({
             message: "Successfully Sent Message",
@@ -36,9 +53,10 @@ exports.sendChat = async (req, res, next) => {
 }
 
 // a refresh button should be what triggers this
-exports.loadChats = async (req, res, next) => {
+exports.loadSentChats = async (req, res, next) => {
     try {
         const chat = await Chat.find({ senderId: req.userId})
+        // const chat = await Chat.find({ senderId: "64d767eec1b864b8d6032672"})
         noData(chat, "Unable to Load Chats")
 
         res.status(200).json({
@@ -49,3 +67,45 @@ exports.loadChats = async (req, res, next) => {
         next(error)
     }
 }
+
+exports.loadReceivedChats = async (req, res, next) => {
+    try {
+        const chat = await Chat.find({ receiverId: req.userId})
+        // const chat = await Chat.find({ receiverId: "64d74acca903ec8a107a9cb5"})
+        noData(chat, "Unable to Load Chats")
+    
+        res.status(200).json({
+            message: "Successfully Fetched Chats",
+            data: chat
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.loadChats = async (req, res, next) => {
+    try {
+        const chats = await Chat.find({
+            $or: [
+                { receiverId: req.userId }, 
+                { senderId: req.userId }
+            ]
+        })
+        // const chats = await Chat.find({
+        //     $or: [
+        //         { receiverId: "64d74acca903ec8a107a9cb5" }, 
+        //         { senderId: "64d74acca903ec8a107a9cb5" }
+        //     ]
+        // })
+        noData(chats, "Unable to Load Chats")
+
+        res.status(200).json({
+            message: "Successfully Fetched Chats",
+            data: chats
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
--- routes/chat.js
+++ routes/chat.js
@@ -4,9 +4,14 @@ const router = express.Router()
 const chatCtrl = require('../controllers/chat')
 const authMid = require('../middlewares/auth')
 
-router.put('/chat/:receiverId', authMid, chatCtrl.sendChat)
+router.put('/chat/:receiverId', chatCtrl.sendChat)
 
-router.get('/chats', authMid, chatCtrl.loadChats)
+router.get('/chats/sent', chatCtrl.loadSentChats)
+
+router.get('/chats/received', chatCtrl.loadReceivedChats)
+
+router.get('/chats', chatCtrl.loadChats)
+
 
 
 module.exports = router
